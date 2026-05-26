729e9cf293f3c13e45bcf3a779c8d2095fee6022
wpwscannerapp <wpwscannerfeed@gmail.com>
2026-05-26 00:29:30 -0400

    Delete collectors directory

--- collectors/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Data collectors package for PWC OSINT Dashboard"""
-
-from .rss_collector import RSSCollector
-from .fire_ems_collector import FireEMSCollector
-
-__all__ = ['RSSCollector', 'FireEMSCollector']
